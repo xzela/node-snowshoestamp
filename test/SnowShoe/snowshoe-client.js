a9156b6311823f7700376a6c9cd9ae8058322a8a
Zeph Lafassett <zeph@lafassett.com>
2014-05-19 21:37:25 -0700

    Adding initial client init tests

--- test/SnowShoe/snowshoe-client.js
+++ test/SnowShoe/snowshoe-client.js
@@ -4,8 +4,36 @@ var should = require('should'),
 
 describe('SnowShoe Client Unit Tests ', function () {
 	describe('Initialize the SnowShoe Client library ', function () {
-		it('It should not explode!', function (done) {
+		it('The tests should should not explode!', function (done) {
 			done();
 		});
+
+		it('should thrown an error if no API key is supplied', function (done) {
+			(function () {
+				var shoe = new SnowShoe();
+			}).should.throw();
+			done();
+		});
+
+		it('should thrown an error if no API secret is supplied', function (done) {
+			(function () {
+				var shoe = new SnowShoe('FakeKey');
+			}).should.throw();
+			done();
+		});
+
+		it('should have the default base URL if no options are supplied', function (done) {
+			var shoe = new SnowShoe('FakeKey', 'FakeSecret');
+			shoe.rootUrl.should.equal('https://beta.snowshoestamp.com/api/v2');
+			done();
+		});
+
+		it('should respect a custom base user if supplied', function (done) {
+			var url = 'http://www.google.com';
+			var shoe = new SnowShoe('FakeKey', 'FakeSecret', {rootUrl: url});
+			shoe.rootUrl.should.equal(url);
+			done();
+		});
+
 	});
 });
