30c20968355f1d9ef8ac8793d94d19e1c1f0ea55
Zeph Lafassett <zeph@lafassett.com>
2014-05-19 21:28:52 -0700

    Adding initial test

---
+++ test/SnowShoe/snowshoe-client.js
@@ -0,0 +1,11 @@
+var should = require('should'),
+	assert = require('assert'),
+	SnowShoe = require('../../lib/SnowShoe');
+
+describe('SnowShoe Client Unit Tests ', function () {
+	describe('Initialize the SnowShoe Client library ', function () {
+		it('It should not explode!', function (done) {
+			done();
+		});
+	});
+});
