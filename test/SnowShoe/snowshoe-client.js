9d943fb7c17f778131f169d093d67a8743a1a1f1
Zeph Lafassett <zeph@lafassett.com>
2014-05-23 08:18:41 -0700

    modifying the constructor api signature. updating tests to reflect changes

--- test/SnowShoe/snowshoe-client.js
+++ test/SnowShoe/snowshoe-client.js
@@ -17,21 +17,32 @@ describe('SnowShoe Client Unit Tests ', function () {
 
 		it('should thrown an error if no API secret is supplied', function (done) {
 			(function () {
-				var shoe = new SnowShoe('FakeKey');
+				var options = {
+					API_KEY: 'FAKE_KEY'
+				};
+				var shoe = new SnowShoe(options);
 			}).should.throw();
 			done();
 		});
 
 		it('should have the default base URL if no options are supplied', function (done) {
-			var shoe = new SnowShoe('FakeKey', 'FakeSecret');
+			var options = {
+				API_KEY: 'FAKE_KEY',
+				API_SECRET: 'FAKE_SECRET'
+			};
+			var shoe = new SnowShoe(options);
 			shoe.url.should.equal('https://beta.snowshoestamp.com/api/v2');
 			done();
 		});
 
 		it('should respect a custom base user if supplied', function (done) {
-			var url = 'http://www.google.com';
-			var shoe = new SnowShoe('FakeKey', 'FakeSecret', {url: url});
-			shoe.url.should.equal(url);
+			var options = {
+				API_KEY: 'FAKE_KEY',
+				API_SECRET: 'FAKE_SECRET',
+				url: 'http://www.google.com'
+			};
+			var shoe = new SnowShoe(options);
+			shoe.url.should.equal(options.url);
 			done();
 		});
 
