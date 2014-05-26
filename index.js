10e0520736f3b67924635f87480d45d30e324780
Zeph Lafassett <zeph@lafassett.com>
2014-05-26 14:31:47 -0700

    Adding requests back to dev dependencies adding callback method and oauth

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 var SnowShoeStamp = require('./lib/SnowShoeStamp'),
 	options = {
-		API_KEY: 'FAKE_API_KEY',
-		APP_SECRET: 'FAKE_APP_SECRET',
+		API_KEY: 'f44441934d35bf14890e',
+		APP_SECRET: '95575808210e67584a79b434979a4fe1003f0f6d',
 		debug: true
 	};
 
 var shoe = SnowShoeStamp(options);
+
+shoe.stamp({
+	user: 12345
+}, function (err, res, body) {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log(body);
+	}
+});
 
 console.log(shoe);
